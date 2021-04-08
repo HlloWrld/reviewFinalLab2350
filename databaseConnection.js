0dd3c322639e424474354a4b5eb0be79e666e024
hllowrld <ira.bcit@protonmail.com>
2021-04-07 19:48:56 -0700

    heroku

--- databaseConnection.js
+++ databaseConnection.js
@@ -4,9 +4,9 @@ const is_heroku = process.env.IS_HEROKU || false;
 
 const dbConfigHeroku = {
 	host: "us-cdbr-east-03.cleardb.com",
-	user: "b1ab7fb2ee03bc",
-	password: "2a484a2d",
-	database: "heroku_3d208ad4bd6f421",
+	user: "befd2f4e424d39",
+	password: "395fb067",
+	database: "heroku_9fafc5446f8d534",
 	multipleStatements: false,
 	namedPlaceholders: true
 };
